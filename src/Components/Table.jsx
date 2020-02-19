2362d2ffe3182761029324a7da4d42e581eea0f0
Jonah Dykhuizen <rjdyk@umich.edu>
2020-02-19 00:04:32 -0500

    initial setup

---
+++ src/Components/Table.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Table.css';
+
+
+class Table extends React.Component {
+    constructor(props){
+
+    }
+
+    render() {
+        return null;
+    };
+}
+
+export default Table;
